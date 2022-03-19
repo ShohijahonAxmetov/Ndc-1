38b864179001158e1dbd21f7ef4e44a5b9023db7
mikamidies <mikamidies04@gmail.com>
2022-03-19 18:01:13 +0500

    ver 2

--- js/app.js
+++ js/app.js
@@ -2,4 +2,14 @@ function scrollHeader(){
     let navbar = document.getElementById('navbar')
     if(this.scrollY >= 50) navbar.classList.add('scroll-header'); else navbar.classList.remove('scroll-header')
   }
-  window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', scrollHeader)
+
+new Swiper('.partnersSwiper', {
+  slidesPerView: 6,
+  spaceBetween: 24,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+})
